7444370b3084eb0fbfc8d348aaa295d1d62e2467
AbbieMutzie <jeruto.abigael1@gmail.com>
2024-08-19 14:19:03 +0300

    User_input Assignment

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Ask user for their name, age and location.
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print("Hello", name, "you are", age, "years old and live in", location,".")
